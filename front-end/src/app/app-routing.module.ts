ba80b1a19dde6c72c97a7fe5892c8a6c6a15f07d
Matheus Foganholo <matheusfoganholo@hotmail.com>
2020-10-28 22:49:02 -0300

    Create page navigation

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ContactCrudComponent } from './views/contact-crud/contact-crud.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "contacts",
+    component: ContactCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
